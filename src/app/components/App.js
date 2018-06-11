4705bb3285d29b4544bd09bc4dcc3b477361d375
Radyuk Tatyana <0400739110>
2018-06-11 16:14:23 +0300

    continued creation of architecture

---
+++ src/app/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../../assets/images/logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
